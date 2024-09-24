7be49e114dc07315ead1d0dffab5160408627f6e
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-09-24 16:27:40 +0300

    add random

--- org/example/app/App.c
+++ org/example/app/App.c
@@ -1,7 +1,10 @@
+//Pashkulsyi Yevhen
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
-{
-
+int main(void) {
+    int i = rand();
+    printf("%d\n", i);
     return 0;
 }
