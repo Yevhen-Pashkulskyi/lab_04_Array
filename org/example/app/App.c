60b51986b9e5710575151b85d9a3e72ddcaaea1d
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-28 10:30:47 +0300

    save

--- org/example/app/App.c
+++ org/example/app/App.c
@@ -3,8 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+int rand_num(int min, int max) {
+    return (rand() % (max - min + 1)) + min;
+}
+
 int main(void) {
-    int i = rand();
-    printf("%d\n", i);
+    for (int i = 0; i < 10; i++) {
+        int t = rand_num(1, 10);
+        int q = rand() % 10;
+        printf("%d ", q);
+        // printf("\n");
+        printf("%d", t);
+    }
     return 0;
 }
