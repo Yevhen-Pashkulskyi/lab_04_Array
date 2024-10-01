c5330fed2cbc6e2cfc532f2b90f08db98dbc5cb3
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-01 14:41:37 +0300

    rename func

--- org/example/app/utils/OutputData.c
+++ org/example/app/utils/OutputData.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void output_data(int arr[]) {
-    for (int i = 0; i < sizeof(arr); i++) {
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 }
