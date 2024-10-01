c05e7a91d6176aef9691cceccf36e966db198d15
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-01 15:24:50 +0300

    created class and func search max and min in array

---
+++ org/example/app/service/Search.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int search_min_array(int *array, int size) {
+    int min = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int search_max_array(int *array, int size) {
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
