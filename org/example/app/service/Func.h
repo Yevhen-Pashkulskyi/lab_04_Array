02fc8b5425f332fcb3f5b9b59d26d97786b2d32b
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-01 14:41:03 +0300

    modified

--- org/example/app/service/Func.h
+++ org/example/app/service/Func.h
@@ -7,7 +7,9 @@ int random_number(int min, int max);
 
 int *array_created(int size, int min, int max);
 
-void output_data(int arr[]);
+void print_array(int arr[], int size);
+
+int *sort_array(int *array, int size);
 
 
 #endif
