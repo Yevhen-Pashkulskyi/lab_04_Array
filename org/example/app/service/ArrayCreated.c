66edcb293c51a9434b1dbf02cf06ecd24c13b13b
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-10-01 14:40:22 +0300

    fix bug

--- org/example/app/service/ArrayCreated.c
+++ org/example/app/service/ArrayCreated.c
@@ -4,6 +4,10 @@
 
 int *array_created(int size, int min, int max) {
     int *arr = malloc(size * sizeof(int));
+    if (arr == NULL) {
+        // perror("malloc");
+        return NULL;
+    }
     for (int i = 0; i < size; i++) {
         arr[i] = random_number(min, max);
     }
